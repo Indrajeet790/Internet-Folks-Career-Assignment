ee761e3fa2ec8fa90b72a2ec1e1e51111a90b114
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-06 14:06:24 +0530

    updated  signIn code

--- routes/userSignIn.js
+++ routes/userSignIn.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const userController=require("../Controllers/user")
 
-router.post("/v1/signIn",userController.SignIn)
+router.post("/signIn",userController.SignIn)
 module.exports=router;
