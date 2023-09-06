60cb26cfbed8c2ff1118d27056f48ce28cd4b4c8
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-06 14:04:43 +0530

    coummunity create and Get My Owned Community routes

--- routes/community.js
+++ routes/community.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const communityController=require("../Controllers/communityController")
 
 router.post("/create",communityController.createCommunity)
+// Get My Owned Community
 router.get("/me/owner",communityController.getOwnCommunities)
 module.exports=router;
