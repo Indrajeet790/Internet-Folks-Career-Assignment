12b821a56f7f87ec66b7a3f22ce499546664d6bd
indrajeet790 <indrajeetkumarnayak3@gmail.com>
2023-09-06 14:06:48 +0530

    updated  signUp code

--- routes/userSignUp.js
+++ routes/userSignUp.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const userController=require("../Controllers/user")
 
-router.post("/v1/sign_up",userController.SignUp)
+router.post("/sign_up",userController.SignUp)
 module.exports=router;
